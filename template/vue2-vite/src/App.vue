2922b1333cb7ef026e9ae5ffb5b1a7f6529b7492
zjtakato <zjtakato@gmail.com>
2023-03-25 19:57:59 +0800

    feat: 新增vue2-vite模板

---
+++ template/vue2-vite/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </div>
+    <div href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello vue2-vite',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
